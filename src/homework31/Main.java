0248f16c02c88b90bf5962daaa714023eb4908a7
NightlyHunter <nightlyhunter@gmail.com>
2017-02-10 17:28:42 +0200

    Homework31

---
+++ src/homework31/Main.java
@@ -0,0 +1,11 @@
+package homework31;
+
+/**
+ * Created by user on 10.02.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Bird.sing());
+
+    }
+}
