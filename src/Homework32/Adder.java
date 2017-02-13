bd92592b1ca9fd1b4cf7f808c6712b688f0804a7
NightlyHunter <nightlyhunter@gmail.com>
2017-02-13 02:10:58 +0200

    Homework22&32begin

---
+++ src/Homework32/Adder.java
@@ -0,0 +1,14 @@
+package Homework32;
+
+/**
+ * Created by user on 13.02.2017.
+ */
+public class Adder {
+    public static boolean chek(Integer a,Integer b){
+        boolean chek=false;
+        if (a>=b){
+            chek=true;
+        }
+        return chek;
+    }
+}
---
+++ src/Homework32/Arithmetic.java
@@ -0,0 +1,13 @@
+package Homework32;
+
+/**
+ * Created by user on 13.02.2017.
+ */
+public class Arithmetic {
+    public static int add(Integer a, Integer b) {
+        int sum;
+        sum = a + b;
+        return sum;
+    }
+}
+
