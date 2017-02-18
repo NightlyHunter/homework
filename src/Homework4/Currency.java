ac3c33159e0fa47cf8cb21aca7b28485b2cf460e
NightlyHunter <nightlyhunter@gmail.com>
2017-02-18 12:31:14 +0200

    Homework4 start

---
+++ src/Homework4/Currency.java
@@ -0,0 +1,8 @@
+package Homework4;
+
+/**
+ * Created by user on 18.02.2017.
+ */
+public enum Currency {
+    USD,EUR
+}
---
+++ src/Homework4/User.java
@@ -0,0 +1,7 @@
+package Homework4;
+
+/**
+ * Created by user on 18.02.2017.
+ */
+public class User {
+}
