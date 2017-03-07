983756d50d46de376868f7cfbe799f5974806c75
NightlyHunter <nightlyhunter@gmail.com>
2017-03-07 22:11:38 +0200

    Homework4 start

---
+++ src/Homework4/BankSystemImpl.java
@@ -0,0 +1,26 @@
+package Homework4;
+
+/**
+ * Created by user on 07.03.2017.
+ */
+public class BankSystemImpl implements BankSystem {
+    @Override
+    public void withdrawOfUser(User user, int amount) {
+
+    }
+
+    @Override
+    public void fundUser(User user, int amount) {
+
+    }
+
+    @Override
+    public void transferMoney(User fromUser, User toUser, int amount) {
+
+    }
+
+    @Override
+    public void paySalary(User user) {
+
+    }
+}
