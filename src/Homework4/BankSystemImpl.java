4792c3666ed15cf8ca686de3e03d76c61be98618
NightlyHunter <nightlyhunter@gmail.com>
2017-03-09 21:41:06 +0200

    Homework4

--- src/Homework4/BankSystemImpl.java
+++ src/Homework4/BankSystemImpl.java
@@ -10,21 +10,27 @@ public class BankSystemImpl implements BankSystem {
             user.setBalance(user.getBalance()-amount-amount*user.getBank().getCommission(amount));
 
         }else System.out.println("Error!!!");
+        System.out.println("Balance after withdrawal="+user.getBalance());
     }
 
     @Override
     public void fundUser(User user, int amount) {
-
-
+        if (amount<=user.getBank().getLimitOfFunding()){
+            user.setBalance(user.getBalance()+amount-amount*user.getBank().getCommission(amount));
+        }else System.out.println("Error!!!");
+        System.out.println("Balance after funding="+user.getBalance());
     }
 
     @Override
     public void transferMoney(User fromUser, User toUser, int amount) {
-
+        fromUser.setBalance(fromUser.getBalance()-amount-amount*fromUser.getBank().getCommission(amount));
+        toUser.setBalance(fromUser.getBalance()+amount);
+        System.out.println("After transfer: balance of "+fromUser+"="+fromUser.getBalance()+"\nBalance of "+toUser+"="+toUser.getBalance());
     }
 
     @Override
     public void paySalary(User user) {
-
+        user.setBalance(user.getBalance()+user.getSalary());
+        System.out.println("Balance after salary="+user.getBalance());
     }
 }
