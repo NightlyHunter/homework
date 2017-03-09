9ec0e53f76aa26b5eef72e758c4430a8c072e305
NightlyHunter <nightlyhunter@gmail.com>
2017-03-09 22:15:22 +0200

    Homework4

--- src/Homework4/Main.java
+++ src/Homework4/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
         USBank Moneymaker = new USBank(123,"YSA",Currency.USD,3,1000,1,99999999);
         User Kolbasa = new User(1,"Kolbasa",1000,3,"Zopa",1000,Moneymaker);
+        BankSystemImpl Bankii= new BankSystemImpl();
+        Bankii.paySalary(Kolbasa);
     }
 }
